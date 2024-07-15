5ebeb8f17f9b560bfd8d39f3ce717c0e875ef470
Ventel Software <mkdeosm@gmail.com>
2024-07-15 23:18:10 +0200

    update

--- server/vector_management.h
+++
@@ -1,9 +0,0 @@
-#ifndef VECTOR_MANAGEMENT_H
-#define VECTOR_MANAGEMENT_H
-
-#include <windows.h>
-
-// Vector management functions
-void VectorConstructorIterator(void* param_1, uint param_2, int param_3, _func_void_ptr_void_ptr* param_4);
-
-#endif // VECTOR_MANAGEMENT_H
